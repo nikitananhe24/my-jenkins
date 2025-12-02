6c0afa86ca92c6959816f3639395f7427d13f90d
Nikita Nanhe <nikitananhe24@gmail.com>
2025-12-02 23:07:39 +0530

    quality-gate stage added

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -1,24 +1,47 @@
 pipeline {
-    agent {label 'slave'}
+    agent { label 'slave' }
+
     stages {
+
         stage('pull-stage') {
             steps {
-                echo "git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'"
+                git branch: 'main', url: 'https://github.com/Athu1717/my-student-app.git'
             }
         }
+
         stage('Build') {
             steps {
                 sh '/opt/maven/bin/mvn clean package'
             }
         }
+
         stage('test') {
             steps {
-                    echo "test success"
+                withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-cred') {
+                sh '/opt/maven/bin/mvn sonar:sonar'
+             }
+                // sh '''
+                // /opt/maven/bin/mvn sonar:sonar \
+                // -Dsonar.projectKey=project \
+                // -Dsonar.host.url=http://18.221.255.167:9000 \
+                // -Dsonar.login=36bed1c36c3b4fe570faf81a9778bd949e37c13a
+                // '''
+                
             }
         }
+        
+           stage('Quality-gate') {
+            steps {
+                timeout(10) {
+    
+            }
+                waitForQualityGate true
+            }
+        }
+
         stage('deploy') {
             steps {
-                echo "deploy sucess"
+                echo "deploy success"
             }
         }
     }
